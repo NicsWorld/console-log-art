de505ae4b655c077d5ed9316eb1322a530444568
Nic Woods <nicwoods63@gmail.com>
2025-02-27 21:34:28 -0600

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+console.log("HI");
+
+// begin console.log art
+console.log("  _____ _   _  _____ _   _  _____ ");
+console.log(" |_   _| | | ||  ___| \\ | ||  ___|");
+console.log("   | | | |_| || |__ |  \\| || |__  ");
+console.log("   | | |  _  ||  __|| . ` ||  __| ");
+console.log("   | | | | | || |___| |\\  || |___ ");
+console.log("   \\_/ \\_| |_/\\____/\\_| \\_/\\____/ ");
+console.log("                                 ");
+// end console.log art
