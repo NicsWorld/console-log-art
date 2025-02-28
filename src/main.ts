cc13ee81a95c13a2a1ea4489d6e3e2e7ec1fd402
Nic Woods <nicwoods63@gmail.com>
2025-02-27 21:41:37 -0600

    exploring a bit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 console.log("HI");
 
-// begin console.log art
 console.log("  _____ _   _  _____ _   _  _____ ");
 console.log(" |_   _| | | ||  ___| \\ | ||  ___|");
 console.log("   | | | |_| || |__ |  \\| || |__  ");
@@ -8,4 +7,21 @@ console.log("   | | |  _  ||  __|| . ` ||  __| ");
 console.log("   | | | | | || |___| |\\  || |___ ");
 console.log("   \\_/ \\_| |_/\\____/\\_| \\_/\\____/ ");
 console.log("                                 ");
-// end console.log art
+
+const leftfish = "<(o_o<)";
+const rightfish = "(>o_o)>";
+const fish = "<(o_o)>";
+
+console.log(leftfish + " " + fish + " " + rightfish);
+
+let counter = 0;
+let tile = "X";
+for (let i = 0; i < 100; i++) {
+  // delay(1000);
+  setTimeout(() => {
+    counter++;
+    console.log(tile.repeat(counter));
+  }, 5000);
+  // counter++;
+  // console.log(tile.repeat(counter));
+}
